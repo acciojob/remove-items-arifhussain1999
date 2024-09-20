7aa0e19c7fdfd72c426e200efaf13e95fb7111da
Arif Hussain <arifh9821@gmail.com>
2024-09-20 16:29:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+
+
+const button = document.querySelector('input[type="button"]');
+
+button.addEventListener('click',function(){
+  
+  const select = document.getElementById('colorSelect');
+
+const selectedIndex = select.selectedIndex;
+
+select.remove(selectedIndex);
+
+});
